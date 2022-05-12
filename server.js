cee056a5c05619f6a6ab077834a1fa92d3a8c020
Edwards Barboza <M4pX_bc@hotmail.com>
2022-05-12 01:51:38 -0500

    bot medio

---
+++ server.js
@@ -0,0 +1,215 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Que inicie la masacre!!!");
+ });
+ 
+ client.on("message", (message) => {
+    let mark = "492337452875382795"
+    let xthedarkx = "718420728072110152"
+    let alonsotuamigo = "787442639208316928"
+    let alvaro611 = "415235717946408971"
+    let aniana = "499213579942101003"
+    let avengethe3000 = "629802736883466255"
+    let baumin2 = "779106833553948713"
+    let bergames = "723448403639533578"
+    let bonie = "625161573832589332"
+    let captainaster = "749442365101441055"
+    let carontes = "426081666264727562"
+    let cheyo = "822608702552801301"
+    let coush = "613223085046366215"
+    let danteuwu = "860641479814676490"
+    let dine = "789364500577910804"
+    let edwards = "221827658026057728"
+    let eye = "443278552415338496"
+    let fore = "541482615283122176"
+    let gamer = "604825350907887654"
+    let gowther = "384553047864639488"
+    let ham = "370810183342882817"
+    let hisoka = "279392303976808449"
+    let hugofares = "660172694125543456"
+    let isanm = "286728091655471104"
+    let jescherry = "594190655924994049"
+    let juano = "407574493276667906"
+    let karlharnos = "211987335368146947"
+    let cruzado = "247508254886854656"
+    let kikin = "515775636246364165"
+    let laputadeandy = "516985597227696128"
+    let loweey = "661081072875995147"
+    let lazarus = "709122962145411163"
+    let madyuu = "329436700155576321"
+    let makakhow = "707077888175767624"
+    let daniii = "620349113967575070"
+    let mija = "273345468589801473"
+    let jime = "371839268487823360"
+    let muffihf = "361565045714452507"
+    let mysthus = "221062296498077698"
+    let nadine = "757143259293155368"
+    let nettokyioshi = "266392813367721984"
+    let nissy = "704868195856547940"
+    let nodreamsboy = "245743429705793536"
+    let pablojc = "651324513795047428"
+    let pablosmc = "722183880097923143"
+    let papulo = "608418579792265216"
+    let pekhres = "295193045287174164"
+    let pikmin = "395067599815180299"
+    let pingu = "499305195839422464"
+    let quimvfgg = "532243202229665794"
+    let yiro = "386583662734475277"
+    let santixytw = "734251768698896404"
+    let sartigan = "394231432702197771"
+    let srqdrk = "367515926683189258"
+    let jhonplay = "439142488477925380"
+    let trapitomojado = "734051755742199868"
+    let vickyveita = "562806769101963274"
+    let xkhaliber = "329565796055515136"
+    let xlinna = "599363884096159759"
+    let xpatrick = "544274826081206285"
+    let xskumbag = "788245890761883678"
+    let yulyi = "239053449491120129"
+    let zantyhago = "452612915267371039"
+    let zdantez = "857302646629072906"
+    let zektrum = "315675056112009226"
+    let zghz = "638222773260648453"
+    let zpudin = "578391124557824000"
+    let fandelafelicidad = "353875141513576457"
+    let juan = "711652526315012198"
+    let rocketdemon = "516985597227696128"
+    let souldream = "565395869981999126"
+    let nachoide = "401088131975217163"
+    let santamaria = "484401597049470989"
+
+    if(message.content.startsWith("!Listassss")) {
+     message.channel.send("Los tributos enlistados son: ");
+     message.channel.send("<@" + mark + ">");
+     message.channel.send("<@" + xthedarkx + ">");
+     message.channel.send("<@" + alonsotuamigo + ">");
+     message.channel.send("<@" + alvaro611 + ">");
+     message.channel.send("<@" + aniana + ">");
+     message.channel.send("<@" + avengethe3000 + ">");
+     message.channel.send("<@" + baumin2 + ">");
+     message.channel.send("<@" + bergames + ">");
+     message.channel.send("<@" + bonie + ">");
+     message.channel.send("<@" + captainaster + ">");
+     message.channel.send("<@" + carontes + ">");
+     message.channel.send("<@" + cheyo + ">");
+     message.channel.send("<@" + coush + ">");
+     message.channel.send("<@" + danteuwu + ">");
+     message.channel.send("<@" + dine + ">");
+     message.channel.send("<@" + edwards + ">");
+     message.channel.send("<@" + eye + ">");
+     message.channel.send("<@" + fore + ">");
+     message.channel.send("<@" + gamer + ">");
+     message.channel.send("<@" + gowther + ">");
+     message.channel.send("<@" + ham + ">");
+     message.channel.send("<@" + hisoka + ">");
+     message.channel.send("<@" + hugofares + ">");
+     message.channel.send("<@" + isanm + ">");
+     message.channel.send("<@" + jescherry + ">");
+     message.channel.send("<@" + juano + ">");
+     message.channel.send("<@" + karlharnos + ">");
+     message.channel.send("<@" + cruzado + ">");
+     message.channel.send("<@" + kikin + ">");
+     message.channel.send("<@" + danteuwu + ">");
+     message.channel.send("<@" + laputadeandy + ">");
+     message.channel.send("<@" + loweey + ">");
+     message.channel.send("<@" + lazarus + ">");
+     message.channel.send("<@" + madyuu + ">");
+     message.channel.send("<@" + makakhow + ">");
+     message.channel.send("<@" + daniii + ">");
+     message.channel.send("<@" + mija + ">");
+     message.channel.send("<@" + jime + ">");
+     message.channel.send("<@" + muffihf + ">");
+     message.channel.send("<@" + mysthus + ">");
+     message.channel.send("<@" + nadine + ">");
+     message.channel.send("<@" + nettokyioshi + ">");
+     message.channel.send("<@" + nissy + ">");
+     message.channel.send("<@" + nodreamsboy + ">");
+     message.channel.send("<@" + pablojc + ">");
+     message.channel.send("<@" + pablosmc + ">");
+     message.channel.send("<@" + papulo + ">");
+     message.channel.send("<@" + pekhres + ">");
+     message.channel.send("<@" + pikmin + ">");
+     message.channel.send("<@" + pingu + ">");
+     message.channel.send("<@" + quimvfgg + ">");
+     message.channel.send("<@" + yiro + ">");
+     message.channel.send("<@" + santixytw + ">");
+     message.channel.send("<@" + sartigan + ">");
+     message.channel.send("<@" + srqdrk + ">");
+     message.channel.send("<@" + jhonplay + ">");
+     message.channel.send("<@" + trapitomojado + ">");
+     message.channel.send("<@" + vickyveita + ">");
+     message.channel.send("<@" + xkhaliber + ">");
+     message.channel.send("<@" + xlinna + ">");
+     message.channel.send("<@" + xpatrick + ">");
+     message.channel.send("<@" + xskumbag + ">");
+     message.channel.send("<@" + yulyi + ">");
+     message.channel.send("<@" + zantyhago + ">");
+     message.channel.send("<@" + zdantez + ">");
+     message.channel.send("<@" + zektrum + ">");
+     message.channel.send("<@" + zghz + ">");
+     message.channel.send("<@" + zpudin + ">");
+     message.channel.send("<@" + fandelafelicidad + ">");
+     message.channel.send("<@" + juan + ">");
+     message.channel.send("<@" + rocketdemon + ">");
+     message.channel.send("<@" + souldream + ">");
+     message.channel.send("<@" + nachoide + ">");
+     message.channel.send("<@" + santamaria + ">");
+    }
+
+    if(message.content.startsWith("!ip")) {
+      message.channel.send("xxxxxxxxxxxxxx");
+    }
+
+    if(message.content.startsWith("!vivos")) {
+      message.channel.send("Los tributos vivos son: ");
+      message.channel.send("```N°1: _xXTheDarkXx_\nN°2: Edwards_BC\nN°3: iGqmer\nN°4: Isanm\nN°5: JesCherry\nN°6: Mysthus\nN°7: Nadine\nN°8: oOMilkShakeOo\nN°9: Pablo_Castillo\nN°10: PabloSMC\nN°11: SrDqrk\nN°12: Zantyhago\nN°13: Zektrum\nN°14: Nissyxd```");
+    } 
+
+    if(message.content.startsWith("!tiesos")) {
+      message.channel.send("Los tributos tiesos son: ");
+      message.channel.send("```N°1: Baumin2\nN°2: bulkuro\nN°3: Kalharnos\nN°4: gowther\nN°5: HisokaShy\nN°6: Komizu\nN°7: MaDyuu\nN°8: Michael30100\nN°9: Pekhres\nN°10: RocketDemond\nN°11: FastHard\nN°12: Alvarooo611\nN°13: Aniana\nN°14: Avengethe3000\nN°15: BlqckWxpGuy\nN°16: Carontes\nN°17: CharlotteM17\nN°18: Cheryo_\nN°19: LilIsaacSempai\nN°20: Eltito2\nN°21: Janix\nN°22: kikin37\nN°23: LordKnight\nN°24: MrsChhocolat\nN°25: Nodreamsboy\nN°26: PAPULOHD\nN°27: rusherboy\nN°28: Santixytw\nN°29: xSkumbag\nN°30: Yulyi\nN°31: zdante\nN°32: zGhz\nN°33: zPudin\nN°34: zLazarus\nN°35: SoulDream\nN°36: Nachoide\nN°37: LaKuroXD\nN°38: yatoshin\nN°39: huevos1\nN°40: HiiroF\nN°41: XxdxnixX\nN°42: XxdxnixX\nN°43: bergameslj3```");
+    }
+          
+    if(message.content.startsWith("!dificultades")) {
+      message.channel.send("```**Normal**\nN°1: Todos los animales ahora sueltan polvo de hueso\nN°2: El agua envenena\nN°3: Los zombies tienen velocidad 3\nN°4: Los phantoms son enormes y dan cegera en un radio de 20x20 bloques\nN°5: Todos los creepers son electricos\nN°6: Los calamares normales y luminosos dan cegera si son intimidados\nN°7: Los ajolotes se comportan como phantoms bajo el agua\nN°8: Los murcielagos son venenosos```");
+      message.channel.send("```**Hard**\nN°1: Ghast Demoniaco X20 De ratio de explosion\nN°2: Cada dia aumenta en 1% la probabilidad de falla del Totem\nN°3: Los golems, Abejas, Lobos, Llamas, Osos polares, Arañas son agresivos en todo momento\nN°4: Todos los Drowned tienen tridentes\nN°5: Los disparos de los Ghast ahora persiguen a los jugadores\nN°6: Los peces te disparan como los guardianes\nN°7: Te cae un rayo si tradeas con los aldeanos\nN°8: Las camas explotan si duermes\nN°9: Los pufferfish explotan al contacto\nN°10: Los ajolotes te quitan 5 slots de corazones cuando estas cerca de ellos\nN°11: Los espiritus de piedra ahora aparecen por defecto```");
+      message.channel.send("```**Hidden**\nN°1: Los Vex explotan al contacto\nN°2: Los esqueletos dan cegera y tienen arco Power 2\nN°3: Las arañas, arañas de cueva, los silverfish y los Vindicator tienen Velocidad 3 o Fuerza 5\nN°4: Los totem al inicio del dia tienen 90% de Falla durante 3 horas\nN°5: Al azar, el servidor coloca una hora de fallta de totem al 100%\nN°6: Caballo cuando le pegas se prende fuego\nN°7: Axolotte dan cegera\nN°8: Los cerdos ahora se convierten en Hoglins\nN°9: Los loros estan en llamas y si te asercas a 1 bloque xe distancia te dan fuego\nN°10: Los zorros disparan bolas de fuego\nN°11: ??????????\nN°12: ??????????\nN°13: ??????????\nN°14: ??????????\nN°15: ??????????\nN°16: ??????????\nN°17: ??????????\nN°18: ??????????```");
+      message.channel.send("```**Blood Gift**\nN°1: Posivilidad de revivir a 50% de los muertos\nN°2: Ligar tu alma con otra persona```");
+    }
+
+    if(message.content.startsWith("!info")) {
+      message.channel.send("Server de Minecraft Vanilla + **PERMADEATH 3** Datapack\n\nVersiÃ³n: 1.17.1\nIP: xxxxxxxxxxxxxx\nPremium | No premium\n\n**Datapacks:**\n```\nPermadeath 3 Datapack\n?????\n?????\n```");
+    }
+
+    if(message.content.startsWith("!mi novia me hizo cornudo?")) {
+      message.channel.send("Messi");
+    }
+
+    if(message.content.startsWith("!si o no?")) {
+      message.channel.send("Simon");
+    }
+
+    if(message.content.startsWith("!me amas?")) {
+        message.channel.send("Nelson");
+    }
+
+    if(message.content.startsWith("!me voy a morir?")) {
+      message.channel.send("Si");
+      message.channel.send("Es broma, pero si quieres no es broma");
+    }
+
+    if(message.content.startsWith("!dame tu bendicion")) {
+      message.channel.send("Amén hermano");
+    }
+
+    if(message.content.startsWith("!permabot")) {
+      message.channel.send("```Comandos serios: \n!ip : Muestra la IP del servidor\n!tiesos: Muestra los muertos actuales del servidor\n!vivos: Muestra los vivos actuales del servidor\n!dificultades: Muestra todas las dificultades\n!info: Muestra informacion detallada del servidor```");
+      message.channel.send("```Comandos serios'snt: \n!mi novia me hizo cornudo?\n!si o no?\n!me amas?\n!me voy a morir?\n!dame tu bendicion```");
+      message.channel.send("```!permabot: Muestra todos los comandos disponibles actualmente ♥ ```");
+    }
+
+  });
+
+ client.login("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
